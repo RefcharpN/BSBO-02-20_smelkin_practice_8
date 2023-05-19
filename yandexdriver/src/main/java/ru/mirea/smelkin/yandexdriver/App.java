40b0731a0303a0cd2bfb65886525b3a0e27fe2fb
smelkin_nikita <nik24sml2000@gmail.com>
2023-05-19 16:53:14 +0300

    практика 8

---
+++ yandexdriver/src/main/java/ru/mirea/smelkin/yandexdriver/App.java
@@ -0,0 +1,13 @@
+package ru.mirea.smelkin.yandexdriver;
+
+import android.app.Application;
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "cdfb0806-26d2-4aff-a5a5-c10899d4ad5c";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
